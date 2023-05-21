71dd9f161427eb5d40689f74f73e7d522b61326d
Vlad B <belousov7vlad@gmail.com>
2023-05-21 17:59:58 +0300

    map fix key

--- frontend/src/components/Ymap/YandexMap.jsx
+++ frontend/src/components/Ymap/YandexMap.jsx
@@ -1,26 +1,32 @@
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
-import icon from '../../assets/icons/marker.svg'
-import './yandexMap.sass'
+import { Map, Placemark } from "@pbe/react-yandex-maps";
+import icon from "../../assets/icons/marker.svg";
+import "./yandexMap.sass";
 
 export const YandexMap = () => {
-
-
-
   const defaultState = {
     center: [55.76, 37.64],
-    zoom: 9
+    zoom: 11,
   };
 
   return (
-    <section className='section'>
-    <YMaps>
-      <Map defaultState={defaultState}
-      width={'100%'}
-      height={600}>
-        <Placemark geometry={[55.684758, 37.738521]} 
+    <section className="section">
+      <Map
+        defaultState={defaultState}
+        width={"100%"}
+        height={600}
+       
+ 
+      >
+        <Placemark
+          geometry={[55.684758, 37.738521]}
+          options={{
+            iconLayout: "default#image",
+            iconImageSize: [40, 40],
+            iconImageHref: icon,
+            iconColor: "#FFFFFF",
+          }}
         />
       </Map>
-    </YMaps>
     </section>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./sass/@globals.sass";
 import { BrowserRouter } from "react-router-dom";
+import { YMaps } from "@pbe/react-yandex-maps";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <YMaps
+        query={{
+          apikey: "f546cab1-1f4a-4f9d-bf0f-22100e02ce30",
+          lang: "ru_RU",
+          load: "package.full",
+        }}
+      >
+        <App />
+      </YMaps>
     </BrowserRouter>
   </React.StrictMode>
 );
