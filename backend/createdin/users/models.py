d5d3db98278f2d5a3f3000afd2e8ac7d535624e2
Galitsin Nikolay <NGalitsin@yandex.com>
2023-05-22 08:35:52 +0300

    added user model and 3 proxy user models, swagger doc

--- backend/createdin/users/models.py
+++ backend/createdin/users/models.py
@@ -1,60 +1,144 @@
-import datetime as dt
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.core.exceptions import ValidationError
-
-USER = "user"
-MODERATOR = "moderator"
-ADMIN = "admin"
-ROLES = [
-    ("user", USER),
-    ("moderator", MODERATOR),
-    ("admin", ADMIN)
-]
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-def validate_year(value):
-    year = dt.date.today().year
-    if value > year:
-        raise ValidationError('Проверьте указанный год')
-    return value
+class UserManager(BaseUserManager):
+    """
+    Custom user manager.
+    Validate data and create new user entry.
+    """
+    def _create_user(self, email, password, **kwargs):
+        is_staff = kwargs.pop('is_staff', False)
+        is_superuser = kwargs.pop('is_superuser', False)
+        role = kwargs.pop('role', 'ADMIN')
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            role=role,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            is_active=True,
+            **kwargs,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, **kwargs):
+        """Creating a user."""
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        """Creating a superuser."""
+        return self._create_user(
+            email=email,
+            password=password,
+            role='ADMIN',
+            is_staff=True,
+            is_superuser=True,
+            **kwargs
+        )
 
 
-class User(AbstractUser):
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-    )
-    first_name = models.CharField(
-        max_length=150,
-        blank=True
-    )
-    last_name = models.CharField(
-        max_length=150,
-        blank=True
-    )
-    email = models.EmailField(
-        max_length=254,
-        unique=True
-    )
-    bio = models.TextField(
-        blank=True,
-    )
-    role = models.CharField(
-        max_length=max(len(role) for _, role in ROLES),
-        choices=ROLES,
-        default=USER
-    )
+class User(AbstractBaseUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        RENTER_INDIVIDUAL = 'RENTER_INDIVIDUAL', 'Renter_individual'
+        RENTER_LEGAL = 'RENTER_LEGAL', 'Renter_legal'
+        LANDLORD = 'LANDLORD', 'Landlord'
 
-    @property
-    def is_admin(self):
-        return self.is_staff or self.role == ADMIN
+    base_role = Role.ADMIN
+    role = models.CharField(max_length=30, choices=Role.choices)
+    email = models.EmailField('Email', unique=True, blank=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    REQUIRED_FIELDS = ('role',)
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
 
-    @property
-    def is_moderator(self):
-        return self.role == MODERATOR
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwargs)
 
-    @property
-    def is_user(self):
-        return self.role == USER
+
+class RenterIndividualManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.RENTER_INDIVIDUAL)
+
+
+class RenterIndividual(User):
+
+    base_role = User.Role.RENTER_INDIVIDUAL
+    renter_individual = RenterIndividualManager()
+
+    class Meta:
+        proxy = True
+
+
+@receiver(post_save, sender=RenterIndividual)
+def create_renter_individual_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'RENTER_INDIVIDUAL':
+        RenterIndividualProfile.objects.create(user=instance)
+
+
+class RenterIndividualProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    test = models.IntegerField(null=True, blank=True)
+
+
+class RenterLegalManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.RENTER_LEGAL)
+
+
+class RenterLegal(User):
+
+    base_role = User.Role.RENTER_LEGAL
+    renter_individual = RenterLegalManager()
+
+    class Meta:
+        proxy = True
+
+
+@receiver(post_save, sender=RenterLegal)
+def create_renter_legal_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'RENTER_LEGAL':
+        RenterLegalProfile.objects.create(user=instance)
+
+
+class RenterLegalProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    test = models.IntegerField(null=True, blank=True)
+
+
+class LandlordManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.LANDLORD)
+
+
+class Landlord(User):
+
+    base_role = User.Role.LANDLORD
+    renter_individual = LandlordManager()
+
+    class Meta:
+        proxy = True
+
+
+@receiver(post_save, sender=Landlord)
+def create_landlird_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'LANDLORD':
+        LandlordProfile.objects.create(user=instance)
+
+
+class LandlordProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    test = models.IntegerField(null=True, blank=True)
