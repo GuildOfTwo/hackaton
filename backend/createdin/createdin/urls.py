e694bc24203fedffd717ab6e020e8f3e71312d1c
Трохимец Константин <katrohimets@mail.ru>
2023-05-26 21:31:31 +0300

    add admin headers and image fix

--- backend/createdin/createdin/urls.py
+++ backend/createdin/createdin/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf.urls import url
 from django.views.static import serve
 from django.conf import settings
 from drf_yasg.views import get_schema_view
@@ -29,7 +28,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     # path('', include(router.urls)),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,})
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,})
 ]
 
 
