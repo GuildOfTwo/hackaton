23d7f1c24d03676ad7e769ac7fcb2286d9b98218
Ruslan Gaziev <grinrus@yandex.ru>
2023-06-06 20:55:46 +0300

    Иконки в мобильной версии регистрации

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { Login, Register } from "../../components/Auth";
-import styles from './styles.module.sass'
+import { useState } from 'react';
+import { Login, Register } from '../../components/Auth';
+import styles from './styles.module.sass';
+import useMediaQuery from '../../utils/hooks/useMediaQuery';
 
 export const AuthPage = () => {
   const [toggle, setToggle] = useState(false);
@@ -9,28 +10,39 @@ export const AuthPage = () => {
     setToggle((prevState) => !prevState);
   };
 
+  const isMobile = useMediaQuery('(max-width: 560px)');
+
   return (
     <section className={styles.section}>
       <div className={styles.buttons}>
-        <button
-          className={styles.button}
-          onClick={() => setToggle(false)}
-        >
-          Регистрация
-        </button>
+        {!isMobile ? (
+          <button className={styles.button} onClick={() => setToggle(false)}>
+            Регистрация
+          </button>
+        ) : (
+          <button
+            className={styles.buttonMobileRegister}
+            onClick={() => setToggle(false)}
+          />
+        )}
+
         <div onClick={handleClick} className={styles.toggle}>
           <div className={toggle ? styles.knobActive : styles.knob} />
         </div>
-        <button
-          className={styles.button}
-          onClick={() => setToggle(true)}
-        >
-          Авторизация
-        </button>
+        
+        {!isMobile ? (
+          <button className={styles.button} onClick={() => setToggle(true)}>
+            Авторизация
+          </button>
+        ) : (
+          <button
+            className={styles.buttonMobileLogin}
+            onClick={() => setToggle(true)}
+          />
+        )}
       </div>
 
       {toggle ? <Login /> : <Register />}
-      
     </section>
   );
 };
