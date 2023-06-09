ee75d091cf76f23095f308963ab5232e9d6d77b1
Vlad B <belousov7vlad@gmail.com>
2023-06-09 22:05:50 +0300

    location

--- frontend/src/components/Personal/PersonalPage.jsx
+++ frontend/src/components/Personal/PersonalPage.jsx
@@ -3,7 +3,7 @@ import { FormLandlord } from "./FormLandlord";
 import { AddNewObject } from "./addNewObject";
 import { ObjectsList } from "./objectsList";
 import { FormRenter } from "./FormRenter";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { ButtonBack } from "../ButtonDefault/ButtonBack";
 import { RequestList } from "./RequestList";
@@ -11,6 +11,8 @@ import { RequestLandlordList } from "./RequestLandlordList";
 
 export const PersonalPage = () => {
   const role = localStorage.getItem("role");
+
+  const location = useLocation()
 
   return (
     <div className={styles.element}>
@@ -43,13 +45,13 @@ export const PersonalPage = () => {
         <>
           {" "}
           <nav className={styles.nav}>
-          <NavLink to="/lk/requests" className={({ isActive }) => isActive ? styles.navLinkActive : styles.navLink }>
+          <NavLink to="/lk/requests" className={() => location.pathname == '/lk/requests' ? styles.navLinkActive : styles.navLink }>
               Заявки
             </NavLink>
-            <NavLink exact to="/lk" className={({ isActive }) => isActive ? styles.navLinkActive : styles.navLink }>
+            <NavLink exact to="/lk" className={() => location.pathname == '/lk' ? styles.navLinkActive : styles.navLink }>
               Список объектов
             </NavLink>
-            <NavLink to="/lk/profile" className={({ isActive }) => isActive ? styles.navLinkActive : styles.navLink }>
+            <NavLink to="/lk/profile" className={() => location.pathname == '/lk/profile' ? styles.navLinkActive : styles.navLink }>
               Редактировать профиль
             </NavLink>
           </nav>
